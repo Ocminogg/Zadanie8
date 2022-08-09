3d0f86bb1f8b8f4a56100c0d9cd38be0c9c856e2
Ocminog <ocminogg@gmail.com>
2022-08-09 14:16:05 +0300

    1

--- zadanie8.6.2/Program.cs
+++ zadanie8.6.2/Program.cs
@@ -10,32 +10,8 @@ namespace zadanie8._6._2
     {
         static void Main(string[] args)
         {
-            //zadanie 8.6.1 
-            Random rnd = new Random();
-            List<int> list = new List<int>();
-            for (int i = 0; i < 100; i++)
-            {
-                list.Add(rnd.Next(0, 100));
-            }
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.Write($"{list[i]}; ");
-            }
-            Console.ReadKey();
-            Console.WriteLine();
-            ///////////////
-            for (int i = 0; i < list.Count; i++)
-            {
-                if ((list[i] > 25) && (list[i] < 50))
-                {
-                    list.Remove(list[i]);
-                }
-            }
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.Write($"{list[i]}; ");
-            }
-            Console.ReadKey();
+            //zadanie 8.6.2
+            
         }
     }
 }
