06a6b0b3dc53384c2e9bc20488af7e547694e1de
Ocminog <ocminogg@gmail.com>
2022-08-09 14:13:49 +0300

    1

---
+++ zadanie8.6.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie8._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 8.6.1 
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(rnd.Next(0, 100));
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]}; ");
+            }
+            Console.ReadKey();
+            Console.WriteLine();
+            ///////////////
+            for (int i = 0; i < list.Count; i++)
+            {
+                if ((list[i] > 25) && (list[i] < 50))
+                {
+                    list.Remove(list[i]);
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]}; ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
