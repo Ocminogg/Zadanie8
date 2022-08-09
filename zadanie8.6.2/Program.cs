bdf9c9fd43b1bbba32587900b6ed95c609e42d88
Ocminog <ocminogg@gmail.com>
2022-08-09 15:16:37 +0300

    1

--- zadanie8.6.2/Program.cs
+++ zadanie8.6.2/Program.cs
@@ -11,7 +11,18 @@ namespace zadanie8._6._2
         static void Main(string[] args)
         {
             //zadanie 8.6.2
-            
+            Dictionary<string, string> PhoneBook = new Dictionary<string, string>();
+            //Заполняем словарь PhoneBook
+            while (true)
+            {
+                string[] Num = Console.ReadLine().Split(' ');
+                if (Num[0] != "")
+                {
+                    PhoneBook.Add(Num[0], Num[1]);
+                }
+                else
+                    break;
+            }
         }
     }
 }
