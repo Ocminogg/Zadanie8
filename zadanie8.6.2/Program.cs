a166baca7ef71a7ae84aa5e7c133b85acaf1b600
Ocminog <ocminogg@gmail.com>
2022-08-09 15:33:46 +0300

    2

--- zadanie8.6.2/Program.cs
+++ zadanie8.6.2/Program.cs
@@ -12,7 +12,11 @@ namespace zadanie8._6._2
         {
             //zadanie 8.6.2
             Dictionary<string, string> PhoneBook = new Dictionary<string, string>();
+            PhoneBook.Add("1", "Жуков");
+            PhoneBook.Add("2", "Тигров");
+            PhoneBook.Add("3", "Козырев");
             //Заполняем словарь PhoneBook
+            Console.WriteLine("Введите новые номера и ФИО из вашей Телефонной книги");
             while (true)
             {
                 string[] Num = Console.ReadLine().Split(' ');
@@ -22,6 +26,24 @@ namespace zadanie8._6._2
                 }
                 else
                     break;
+            }
+            //Ищем ФИО по тел
+            Console.WriteLine("Введите номер тел и мы найдем ваш контакт");
+            while (true)
+            {
+                string Phone = Console.ReadLine();
+                string FIO;
+                if (PhoneBook.TryGetValue(Phone,out FIO) == true)
+                {
+                    Console.WriteLine($"Найденный контакт: {FIO}");
+                }
+                else
+                {
+                    Console.WriteLine("Контакт не найден. Чтобы искать другой нажмите 1!");
+                    string con = Console.ReadLine();
+                    if (con == "1") continue;
+                    else break;
+                }
             }
         }
     }
