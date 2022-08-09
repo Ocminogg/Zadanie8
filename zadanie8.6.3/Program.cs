be7b207324eeade00d934af2aef56913b523985d
Ocminog <ocminogg@gmail.com>
2022-08-09 16:01:18 +0300

    1

--- zadanie8.6.3/Program.cs
+++ zadanie8.6.3/Program.cs
@@ -10,7 +10,22 @@ namespace zadanie8._6._3
     {
         static void Main(string[] args)
         {
-
+            HashSet<int> set = new HashSet<int>(new int[] {1,2,3,4,5,10});
+            while (true)
+            {
+                Console.WriteLine("Введите ваше число для добавления");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (set.Contains(num)==false)
+                {
+                    set.Add(num);
+                    Console.WriteLine("Мы добавили ваше число");
+                }
+                else
+                {
+                    Console.WriteLine("Такое число уже есть");
+                    continue;
+                }
+            }
         }
     }
 }
